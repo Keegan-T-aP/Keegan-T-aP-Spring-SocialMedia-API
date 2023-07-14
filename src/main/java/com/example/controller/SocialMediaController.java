039afe3a5d0ef062f68ba21ded0c4207e97bd644
jhigherevature <71463449+jhigherevature@users.noreply.github.com>
2023-07-14 14:26:45 -0600

    Update SocialMediaController.java

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.*;
  * found in readme.md as well as the test cases. You be required to use the @GET/POST/PUT/DELETE/etc Mapping annotations
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
+ *
+ * You are expected to have at least the following methods: getAllMessages, getMessageByID, getMessageByAccount, registerAccount, newMessage, login, deleteMessage.
+ * 
+ * These methods should follow proper naming conventions, and include appropriate arguments. Refer to the tests provided for more details.
  */
-
 @RestController
 public class SocialMediaController {
 
